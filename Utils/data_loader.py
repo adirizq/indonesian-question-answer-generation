b09f2e92d18397ded1e02f5139e291c4f5cfb739
adirizq <rizkyzadadi@gmail.com>
2023-10-17 13:18:24 +0700

    finish ae data loader early version

--- Utils/data_loader.py
+++ Utils/data_loader.py
@@ -6,42 +6,113 @@ import torch.nn as nn
 import multiprocessing
 import pytorch_lightning as pl
 
-from torch.utils.data import TensorDataset, DataLoader
+from tqdm import tqdm
 from transformers import BartTokenizer
+from indobenchmark import IndoNLGTokenizer
+from torch.utils.data import TensorDataset, DataLoader
 
 
 class AnswerExtractionDataModule(pl.LightningDataModule):
 
     def __init__(self, 
                  dataset_name, 
+                 input_type,
+                 output_type,
                  pre_trained_model_name='indobenchmark/indobart-v2', 
                  max_length=512, 
                  batch_size=32, 
-                 recreate=False
+                 recreate=False,
                  ) -> None:
 
         super(AnswerExtractionDataModule, self).__init__()
 
         self.seed = 42
+        self.dataset_name = dataset_name
+        self.input_type = input_type
+        self.output_type = output_type
         self.pre_trained_model_name = pre_trained_model_name
         self.max_length = max_length
         self.batch_size = batch_size
         self.recreate = recreate
+        
+        self.train_dataset_path = f"Datasets/Processed/{dataset_name}/prepared_train.csv"
+        self.valid_dataset_path = f"Datasets/Processed/{dataset_name}/prepared_dev.csv"
+        self.test_dataset_path = f"Datasets/Processed/{dataset_name}/prepared_test.csv"
 
-        self.train_dataset_path = f"Datasets/Processed/{dataset_name}/train.csv"
-        self.dev_dataset_path = f"Datasets/Processed/{dataset_name}/dev.csv"
-
-        self.train_tokenized_pickle = f"Datasets/Tensor/{dataset_name}/train.pkl"
-        self.dev_tokenized_pickle = f"Datasets/Tensor/{dataset_name}/dev.pkl"
+        self.train_tensor_dataset_path = f"Datasets/Tensor/{dataset_name}_{input_type}_to_{output_type}/train.pt"
+        self.valid_tensor_dataset_path = f"Datasets/Tensor/{dataset_name}_{input_type}_to_{output_type}/dev.pt"
+        self.test_tensor_dataset_path = f"Datasets/Tensor/{dataset_name}_{input_type}_to_{output_type}/test.pt"
 
         self.load_data()
     
 
     def load_data(self):
-        if os.path.exists(self.train_tokenized_pickle) and os.path.exists(self.dev_tokenized_pickle) and not self.recreate:
-            print('\n[ Loading Dataset ]')
+        if os.path.exists(self.train_tensor_dataset_path) and os.path.exists(self.valid_tensor_dataset_path) and os.path.exists(self.test_tensor_dataset_path) and not self.recreate:
             
+            print('\n[ Loading Dataset ]')
+            train_data = torch.load(self.train_tensor_dataset_path)
+            valid_data = torch.load(self.valid_tensor_dataset_path)
+            test_data = torch.load(self.test_tensor_dataset_path)
+            print('[ Load Completed ]')
 
+            return train_data, valid_data, test_data
+        
+        else:
+
+            print('\n[ Processing Dataset ]')
+            self.tokenizer = IndoNLGTokenizer.from_pretrained(self.pre_trained_model_name)
+            self.tokenizer.add_special_tokens({'additional_special_tokens': ['<hl>']})
+
+            # Must add model.resize_token_embeddings(len(tokenizer)) after adding new special tokens
+
+            data_csv = {
+                'train': pd.read_csv(self.train_dataset_path),
+                'dev': pd.read_csv(self.valid_dataset_path),
+                'test': pd.read_csv(self.test_dataset_path),
+            }
+
+            tokenized_data = {
+                'train': [],
+                'dev': [],
+                'test': [],
+            }
+
+            for key, data in data_csv.items():
+
+                input_ids, attention_mask, target_ids = [], [], []
+
+                for (context, context_key_sentence, context_answer, question, answer) in tqdm(data.values.tolist(), desc=f'Tokenizing {key} data'):
+
+                    input_text = locals()[self.input_type]
+                    output_text = locals()[self.output_type]
+
+                    encoded_input = self.tokenizer(f'<s>{input_text}</s>', 
+                                                                  add_special_tokens=True, 
+                                                                  max_length=self.max_length,
+                                                                  padding="max_length",
+                                                                  truncation=True)
+                    
+                    encoded_output =  self.tokenizer(f'<s>{output_text}</s>',
+                                                     add_special_tokens=True, 
+                                                     max_length=self.max_length,
+                                                     padding="max_length",
+                                                     truncation=True)
+
+                    input_ids.append(encoded_input['input_ids'])
+                    attention_mask.append(encoded_input['attention_mask'])
+                    target_ids.append(encoded_output['input_ids'])
+                
+                tensordataset_save_dir = f"Datasets/Tensor/{self.dataset_name}_{self.input_type}_to_{self.output_type}"
+
+                if not os.path.exists(tensordataset_save_dir):
+                    os.makedirs(tensordataset_save_dir)
+
+                tokenized_data[key] = TensorDataset(torch.tensor(input_ids), torch.tensor(attention_mask), torch.tensor(target_ids))
+                torch.save(tokenized_data[key], f'{tensordataset_save_dir}/{key}.pt')
+
+            print('[ Process Completed ]')
+            
+            return tokenized_data['train'], tokenized_data['dev'], tokenized_data['test']
 
 
     def setup(self, stage=None):
