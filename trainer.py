fa4dcd5889f02491eeebbee3dc2ecf99eb1fbc0e
adirizq <rizkyzadadi@gmail.com>
2023-10-18 16:11:05 +0700

    add callbacks and logger

--- trainer.py
+++ trainer.py
@@ -7,7 +7,7 @@ os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
 
 from pytorch_lightning import Trainer, seed_everything
 from pytorch_lightning.callbacks import ModelCheckpoint, TQDMProgressBar, EarlyStopping
-from pytorch_lightning.loggers import TensorBoardLogger, CSVLogger
+from pytorch_lightning.loggers import CSVLogger
 from textwrap import dedent
 
 from Utils.data_loader import AnswerExtractionDataModule
@@ -29,11 +29,21 @@ if __name__ == "__main__":
     data_module = AnswerExtractionDataModule(dataset_name="TyDiQA", input_type="context", output_type='context_answer', batch_size=1, recreate=recreate)
     model = BartAnswerExtraction(tokenizer=data_module.get_tokenizer())
 
+    # Initialize callbacks and progressbar
+    csv_logger = CSVLogger('csv_logs', name=f'logs')
+    checkpoint_callback = ModelCheckpoint(dirpath=f'./checkpoints', monitor='val_loss', mode='min')
+    early_stop_callback = EarlyStopping(monitor='val_loss', min_delta=0.00, check_on_train_epoch_end=1, patience=3, mode='min')
+    tqdm_progress_bar = TQDMProgressBar()
+
     trainer = Trainer(
         accelerator=config['accelerator'],
+        default_root_dir=f'./checkpoints',
+        callbacks=[checkpoint_callback, early_stop_callback, tqdm_progress_bar],
+        logger=[csv_logger],
         max_epochs=5,
         log_every_n_steps=5,
         deterministic=True 
     )
 
     trainer.fit(model, datamodule=data_module)
+    trainer.test(datamodule=data_module, ckpt_path='best')
